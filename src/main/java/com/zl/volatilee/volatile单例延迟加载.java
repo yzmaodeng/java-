587c64a0d0101260e42ceb77a63b5f910bb65065
yzmaodeng <yzmaodeng@outlook.com>
2018-05-26 23:46:10 +0800

    volatile 单例

--- src/main/java/com/zl/volatilee/volatile单例延迟加载.java
+++ src/main/java/com/zl/volatilee/volatile单例延迟加载.java
@@ -1,10 +1,17 @@
 package com.zl.volatilee;
 
 public class volatile单例延迟加载 {
-
-	public static void main(String[] args) {
-		
-
-	}
+	private volatile static volatile单例延迟加载 instance = null;  
+	 private volatile单例延迟加载() {}  
+	 public static volatile单例延迟加载 getInstance() {  
+	  if (instance == null) {  
+	   synchronized (Singleton.class) {// 1  
+	    if (instance == null) {// 2  
+	     instance = new volatile单例延迟加载();// 3  
+	    }  
+	   }  
+	  }  
+	  return instance;  
+	 }  
 
 }
